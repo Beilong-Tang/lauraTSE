0efc5764d5927b128336324b33379a8a571fcc49
beilong tang <bt132@duke.edu>
2025-03-13 21:08:57 +0800

    update

--- laura_gpt_tse_only_clean_output_dm_ac/src/train.py
+++ laura_gpt_tse_only_clean_output_dm_ac/src/train.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys 
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 import random
 import yaml
