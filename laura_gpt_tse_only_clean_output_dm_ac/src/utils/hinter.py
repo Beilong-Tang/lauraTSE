c5f07b638de2a7b3b189799aa7a6b3beabad3b4d
beilong tang <bt132@duke.edu>
2025-03-15 09:43:49 +0800

    update

--- laura_gpt_tse_only_clean_output_dm_ac/src/utils/hinter.py
+++ laura_gpt_tse_only_clean_output_dm_ac/src/utils/hinter.py
@@ -1,24 +1,30 @@
 import torch.distributed
-import logging
 
-HINTED = set()
+HINTED = dict()
 
-
-def hint_once(content, uid, rank=None):
+def hint_once(content, uid, rank=None, times = 1):
     """
     ranks: which rank to output log
+    times: how many times to hint
     """
     _cur_rank = None 
     if torch.distributed.is_initialized():
         _cur_rank = torch.distributed.get_rank()
 
+    if HINTED.get(uid) is None:
+        HINTED[uid] = 0
+
     if (rank is None) or (_cur_rank is None) or _cur_rank == rank:
-        if uid not in HINTED:
-            if _cur_rank is not None:
-                print(f"[HINT_ONCE] Rank {_cur_rank}: {content}")
-            else:
-                print(f"[HINT_ONCE] {content}")
-            HINTED.add(uid)
+
+        # Check if the times exceed the HINTED
+        if HINTED.get(uid) >=times:
+            return 
+        
+        if _cur_rank is not None:
+            print(f"[HINT_ONCE {HINTED.get(uid)+1/times}] Rank {_cur_rank}: {content}")
+        else:
+            print(f"[HINT_ONCE {HINTED.get(uid)+1/times}] {content}")
+        HINTED[uid] = HINTED[uid] + 1
 
 def check_hint(uid) -> bool:
     """
